4a081ac448f8b77d88440f2a72f1906d07e25bad
akaeda510 <moontype510@i.softbank.jp>
2026-09-19 13:54:06 +0900

    feat:mainメソッドを実装

--- calculator.java
+++ calculator.java
@@ -1,1 +1,11 @@
+import java.util.Scanner;
 
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("+, -, x, /, =が使用できます");
+		System.out.print("式：");
+		String input = scanner.nextLine();
+		scanner.close();
+	} 
+}
