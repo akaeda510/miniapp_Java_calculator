567b1c7733f8c36a7222263b33c3b245a832d31c
akaeda510 <moontype510@i.softbank.jp>
2026-09-19 14:43:52 +0900

    Revert "update:partsメソッドの構文ミスがあったため修正"
    This reverts commit 0c244e806450e1a730aed94fee03c26a2ccc89bc.

--- calculator.java
+++ calculator.java
@@ -8,36 +8,13 @@ public class Calculator {
 		String input = scanner.nextLine();
 		scanner.close();
 
-		String[] parts = parts(input);
-		double result = result(parts);
-		System.out.println("結果:" + result);
+		parts(input);
 	} 
 
-	public static String[] parts(String input) {
+	public void parts(String input) {
 		String[] parts = input.split(" ");
-		return parts;
-	}
-
-	public static double result(String[] parts) {
 		String num1 = parts[0];
 		String operator = parts[1];
 		String num2 = parts[2];
-
-		double result = 0;
-		switch (operator) {
-			case "+":
-				result = Double.parseDouble(num1) + Double.parseDouble(num2);
-				break;
-			case "-":
-				result = Double.parseDouble(num1) - Double.parseDouble(num2);
-				break;
-			case "x":
-				result = Double.parseDouble(num1) * Double.parseDouble(num2);
-				break;
-			case "/":
-				result = Double.parseDouble(num1) / Double.parseDouble(num2);
-				break;
-		}
-		return result;
 	}
 }
