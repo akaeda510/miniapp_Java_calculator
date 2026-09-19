a0e11fb881d374f1757096d650fd6ca69415cd17
akaeda510 <moontype510@i.softbank.jp>
2026-09-19 15:15:06 +0900

    feat:mainメソッドを作成
    # 変数
    parts: ユーザーの導入文字列
    result: 結果
    
    # 表記形式
    ```
    +, -, x, /, =が使用できます
    式:
    ```

--- calculator.java
+++ calculator.java
@@ -1,1 +1,14 @@
+import java.util.Scanner;
 
+public class Calculator {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("+, -, x, /, =が使用できます");
+		System.out.print("式：");
+		String input = scanner.nextLine();
+		scanner.close();
+
+		String[] parts = parts(input);
+		double result = result(parts);
+		System.out.println("結果: " + result);
+	} 
