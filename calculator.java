dde7fb25428f7227804cc18981f0d626b4bfa723
akaeda510 <moontype510@i.softbank.jp>
2026-09-24 10:29:04 +0900

    update/表記形式を変更
    表記形式に例題を追加

--- calculator.java
+++ calculator.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
 
 public class Calculator {
+
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		System.out.println("+, -, x, /, =が使用できます");
+		System.out.println("+, -, x, /, =が使用できます (例: 10 + 20 =)");
 		System.out.print("式：");
 		String input = scanner.nextLine();
 		scanner.close();
